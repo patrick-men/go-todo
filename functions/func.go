7d0a515ef34aa8abbbe0c8ca28b6f823fbcc5c92
Patrick M <patrick-mendes@bluewin.ch>
2023-10-06 21:07:10 +0200

    chore: initialize go proj

---
+++ functions/func.go
@@ -0,0 +1,15 @@
+package functions
+
+import (
+	"fmt"
+)
+
+func Test() (s string) {
+	s = "test"
+	fmt.Println(s)
+
+	return s
+}
+
+
+
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"todo/functions"
+)
+
+func main() {
+    functions.Test()
+}
